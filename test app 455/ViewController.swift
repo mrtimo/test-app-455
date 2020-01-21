eecb1412bfd87041d6308328fee5d81c7c96c920
Tim Olsen <olsen.tim@gmail.com>
2020-01-21 10:30:50 -0800

    done with the app

--- test app 455/ViewController.swift
+++ test app 455/ViewController.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel1: UILabel!
+    
+    @IBOutlet weak var messageLabel2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
+        messageLabel1.text = " "
+        messageLabel2.text = " "
     }
 
-
+    @IBAction func googlePressed(_ sender: UIButton) {
+        messageLabel1.text = " "
+        messageLabel2.text = "hi"
+        messageLabel2.textColor = UIColor.blue
+    }
+    
+    @IBAction func gonzagaPressed(_ sender: UIButton) {
+        messageLabel1.text = " "
+        messageLabel2.text = " "
+        
+    }
+    
+    @IBAction func clearPressed(_ sender: UIButton) {
+        messageLabel1.text = " "
+        messageLabel2.text = " "
+    }
+    
+    
+    
+    
 }
 
